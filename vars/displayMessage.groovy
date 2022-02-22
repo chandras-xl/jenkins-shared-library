92feb0f8658fffa8852354d026a501ff54c8da2e
chandras-xl <chandrashekhar.s@agiliad.com>
2022-02-22 13:04:21 +0530

    initial commit

---
+++ vars/displayMessage.groovy
@@ -0,0 +1,39 @@
+#!groovy
+
+def call(String msg){
+    println("Hello! $msg")
+
+    pipeline{
+        agent any 
+
+        stages{
+            stage('build'){
+                sh '''
+                    echo "Building the pipeline
+                '''
+            }
+            stage('Test'){
+                sh '''
+                    echo "Performing Unit test
+                '''
+            }
+            stage('Deploy'){
+                sh '''
+                    echo "Deploying"
+                '''
+            }
+        }
+        post{
+            success{
+                slackSend channel: '#jenkinsci',
+                          color: 'good',
+                          message: "Executed the job ${currentBuild.fullDisplayName}"       
+            }
+            failure{
+                slackSend channel: '#jenkinsci',
+                          color: 'danger',
+                          message: "Failed to execute the job ${currentBuild.fullDisplayName}"
+            }
+        }
+    }
+}
